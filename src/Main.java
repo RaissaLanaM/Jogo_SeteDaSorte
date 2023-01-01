e9929eb2bf4e3e52821bb2ac269f099c027480db
RaissaLanaM <raissa142lanamc@gmail.com>
2023-01-01 20:10:04 -0300

    Jogo 7 da Sorte

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import view.FrmJogo;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame tela = new JFrame();
+        tela.setTitle("Jogo 7 da Sorte");
+        tela.setSize(500,500);
+        tela.setLocationRelativeTo(null);
+        tela.setResizable(false);
+        tela.setContentPane(new FrmJogo() .getPanelGame());
+        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        tela.setVisible(true);
+    }
+}
